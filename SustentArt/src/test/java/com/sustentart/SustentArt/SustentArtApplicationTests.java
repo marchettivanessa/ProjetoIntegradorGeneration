f6e608b9704404ad552bb147403ec50a9c7458be
Vanessa Marchetti <vanessa.b.marchetti@gmail.com>
2020-10-05 15:19:37 -0300

    Criando minha primeira tabela

---
+++ SustentArt/src/test/java/com/sustentart/SustentArt/SustentArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sustentart.SustentArt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SustentArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
